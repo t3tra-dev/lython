5d5d7ad02d9b9feeb822f214e91c7878122d8f01
t3tra-dev <admin@t3tra.net>
2025-02-08 01:18:15 +0900

    built the framework of `BoolOpVisitor`

--- pyc/codegen/visitors/boolop.py
+++ pyc/codegen/visitors/boolop.py
@@ -1,3 +1,6 @@
+import ast
+
+from pyc.codegen.ir.builder import IRBuilder
 from .base import BaseVisitor
 
 
@@ -5,5 +8,23 @@ class BoolOpVisitor(BaseVisitor):
     """
     ```asdl
     boolop = And | Or
+    ```
     """
-    pass
+    def __init__(self, builder: IRBuilder):
+        super().__init__(builder)
+
+    def visit_And(self, node: ast.And) -> None:
+        """
+        ```asdl
+        And
+        ```
+        """
+        raise NotImplementedError("And boolop not implemented")
+
+    def visit_Or(self, node: ast.Or) -> None:
+        """
+        ```asdl
+        Or
+        ```
+        """
+        raise NotImplementedError("Or boolop not implemented")
