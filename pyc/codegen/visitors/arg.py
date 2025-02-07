3df2dd71682626aa9b9b5b7bd3cd242a9f75bc8c
t3tra-dev <admin@t3tra.net>
2025-02-08 01:42:06 +0900

    fix

--- pyc/codegen/visitors/arg.py
+++ pyc/codegen/visitors/arg.py
@@ -9,6 +9,7 @@ class ArgVisitor(BaseVisitor):
     ```asdl
     arg = (identifier arg, expr? annotation, string? type_comment)
            attributes (int lineno, int col_offset, int? end_lineno, int? end_col_offset)
+    ```
     """
     def __init__(self, builder: IRBuilder):
         super().__init__(builder)
