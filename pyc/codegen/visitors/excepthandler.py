f549d23b9a84352f5e58e47332db26e8e9468064
t3tra-dev <admin@t3tra.net>
2025-02-08 01:37:38 +0900

    fix

--- pyc/codegen/visitors/excepthandler.py
+++ pyc/codegen/visitors/excepthandler.py
@@ -9,6 +9,7 @@ class ExceptHandlerVisitor(BaseVisitor):
     ```asdl
     excepthandler = ExceptHandler(expr? type, identifier? name, stmt* body)
                     attributes (int lineno, int col_offset, int? end_lineno, int? end_col_offset)
+    ```
     """
     def __init__(self, builder: IRBuilder):
         super().__init__(builder)
