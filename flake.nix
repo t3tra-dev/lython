68e5f881f22abd0291410a89906c6fdd46948bad
take-cheeze <takechi101010@gmail.com>
2025-02-09 23:37:13 +0900

    Add CI

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.11";
+  };
+    
+  outputs = { self, nixpkgs }:
+  let 
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in {
+    packages.x86_64-linux.build = pkgs.stdenv.mkDerivation {
+      name = "pyc";
+      nativeBuildInputs = with pkgs; [
+        boehmgc
+        bun
+        deno
+        llvmPackages_19.clangUseLLVM
+        pkg-config
+        python313FreeThreading
+        uv
+      ];
+    };
+
+    packages.x86_64-linux.default = self.packages.x86_64-linux.build;
+  };
+}
