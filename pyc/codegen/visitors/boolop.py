5775652e9833b7832962ce291c724e643b4e9aa5
t3tra-dev <admin@t3tra.net>
2025-02-10 17:04:38 +0900

    refactor(visitor): Improve boolean and comparison operation visitors

--- pyc/codegen/visitors/boolop.py
+++ pyc/codegen/visitors/boolop.py
@@ -2,6 +2,7 @@ import ast
 
 from pyc.codegen.ir.builder import IRBuilder
 from .base import BaseVisitor
+from .expr import ExprVisitor
 
 
 class BoolOpVisitor(BaseVisitor):
@@ -13,18 +14,43 @@ class BoolOpVisitor(BaseVisitor):
     def __init__(self, builder: IRBuilder):
         super().__init__(builder)
 
-    def visit_And(self, node: ast.And) -> None:
+    def visit(self, node: ast.BoolOp, expr_visitor: ExprVisitor) -> str:
         """
+        Determine the type of boolean operation and process it.
+        """
+        if len(node.values) != 2:
+            raise NotImplementedError("Multi-value BoolOp not fully supported")
+
+        left_val = expr_visitor.visit(node.values[0])
+        right_val = expr_visitor.visit(node.values[1])
+
+        if isinstance(node.op, ast.And):
+            return self.visit_And(left_val, right_val)
+        elif isinstance(node.op, ast.Or):
+            return self.visit_Or(left_val, right_val)
+        else:
+            raise NotImplementedError(f"Unsupported BoolOp: {type(node.op).__name__}")
+
+    def visit_And(self, left_val: str, right_val: str) -> str:
+        """
+        Process the 'And' boolean operation.
+
         ```asdl
         And
         ```
         """
-        raise NotImplementedError("And boolop not implemented")
+        temp = self.builder.get_temp_name()
+        self.builder.emit(f"  {temp} = and i32 {left_val}, {right_val}")
+        return temp
 
-    def visit_Or(self, node: ast.Or) -> None:
+    def visit_Or(self, left_val: str, right_val: str) -> str:
         """
+        Process the 'Or' boolean operation.
+
         ```asdl
         Or
         ```
         """
-        raise NotImplementedError("Or boolop not implemented")
+        temp = self.builder.get_temp_name()
+        self.builder.emit(f"  {temp} = or i32 {left_val}, {right_val}")
+        return temp
