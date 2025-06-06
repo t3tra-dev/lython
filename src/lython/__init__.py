a6ae864c3ba80fb4cfb5677d8b6246c342c2c5d3
t3tra-dev <admin@t3tra.net>
2025-06-06 10:58:32 +0900

    re: init

---
+++ src/lython/__init__.py
@@ -0,0 +1,39 @@
+"""
+Lython - Python compiler toolchain based on LLVM
+~~~~~~~~~~~~~~~~~~~
+
+:copyright: (c) 2024-present t3tra
+:license: MIT, see LICENSE for more details.
+
+"""
+
+__title__ = "lython"
+__author__ = "t3tra"
+__license__ = "MIT"
+__copyright__ = "Copyright 2024-present t3tra"
+__version__ = "0.1.0"
+
+__path__ = __import__("pkgutil").extend_path(__path__, __name__)
+
+import logging
+from typing import Literal, NamedTuple
+
+
+__all__ = []
+
+
+class VersionInfo(NamedTuple):
+    major: int
+    minor: int
+    micro: int
+    releaselevel: Literal["alpha", "beta", "candidate", "final"]
+    serial: int
+
+
+version_info: VersionInfo = VersionInfo(
+    major=0, minor=0, micro=1, releaselevel="final", serial=0
+)
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+del logging, NamedTuple, Literal, VersionInfo
