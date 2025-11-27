646f6dd3cf85a474d5dd6cead6fd64ea6732391e
t3tra <t3tra-dev@users.noreply.github.com>
2025-11-27 20:57:29 +0900

    fix type stubs

--- examples/fib_prim.py
+++ examples/fib_prim.py
@@ -5,11 +5,11 @@ prim_one = to_prim(1, i8)
 prim_two = to_prim(2, i8)
 
 
-@native
+@native(gc="none")
 def fib(n: i8) -> i8:
     if n <= prim_one:
         return n
     return fib(n - prim_one) + fib(n - prim_two)
 
 
-print(from_prim(fib(to_prim(10, i8))))
+print(from_prim(fib(to_prim(35, i8))))
--- src/lyrt/__init__.pyi
+++ src/lyrt/__init__.pyi
@@ -1,11 +1,17 @@
-from typing import Any, Callable, TypeVar
+from typing import Any, Callable, Literal, TypeVar
 
 from . import prim
 
 __all__ = ["prim", "native", "to_prim", "from_prim"]
 
-T = TypeVar("T", bound=prim.prim)
+T = TypeVar("T")
+PrimT = TypeVar("PrimT", bound=prim.prim)
 
-def native(func: Callable[..., Any]) -> Callable[..., Any]: ...
-def to_prim(value: object, prim_type: type[T]) -> T: ...
+def native(
+    func: Callable[..., Any] | None = ...,
+    /,
+    *,
+    gc: Literal["none", "shadow-stack", "rc"] = ...,
+) -> Callable[..., T] | Callable[[Callable[..., T]], Callable[..., T]]: ...
+def to_prim(value: object, prim_type: type[PrimT]) -> PrimT: ...
 def from_prim(prim_value: prim.prim) -> object: ...
