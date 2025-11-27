dfe4131ea53709de470ea88c49509fe03f56d207
t3tra <t3tra-dev@users.noreply.github.com>
2025-11-28 03:41:51 +0900

    refactor: improve type hints

--- src/lyrt/__init__.pyi
+++ src/lyrt/__init__.pyi
@@ -1,4 +1,4 @@
-from typing import Any, Callable, Literal, TypeVar
+from typing import Callable, Literal, TypeVar
 
 from . import prim
 
@@ -6,12 +6,11 @@ __all__ = ["prim", "native", "to_prim", "from_prim"]
 
 T = TypeVar("T")
 PrimT = TypeVar("PrimT", bound=prim.prim)
+PrimF = TypeVar("PrimF", bound=Callable[..., prim.prim])
 
 def native(
-    func: Callable[..., Any] | None = ...,
-    /,
     *,
     gc: Literal["none", "shadow-stack", "rc"] = ...,
-) -> Callable[[Callable[..., T]], Callable[..., T]]: ...
+) -> Callable[[PrimF], PrimF]: ...
 def to_prim(value: object, prim_type: type[PrimT]) -> PrimT: ...
 def from_prim(prim_value: prim.prim) -> object: ...
